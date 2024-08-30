64c420a2f76b1ed14eca3f3d8e85159207aed7d5
Your Name <you@example.com>
2024-08-30 19:45:51 +0400

    Добавьте файлы проекта.

---
+++ Tahir/Program.cs
@@ -0,0 +1,66 @@
+﻿
+
+#region
+//int[] nums = { 7, 8, 10, 25, 19 };
+
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    if (nums[i] < 10)
+//    {
+//        Console.WriteLine(nums[i]);
+//    }
+//}
+#endregion
+
+
+#region
+string word = "Veten";
+for (int i = 0; i < word.Length; i++)
+{
+    if (word[i] == 'a')
+    {
+        Console.WriteLine(word);
+    }
+    else 
+    {
+        Console.WriteLine("A herfi olan soz yoxdur");
+    }
+}
+
+#endregion
+
+
+#region
+//string[] words = { "Salam", "Top", "Gul" };
+
+//for (int i = 0; i < words.Length; i++)
+//{
+//    if (words[i].Length>4)
+//    {
+//        Console.WriteLine(words[i]);
+//    }
+//}
+#endregion
+
+
+#region
+//int[] nums = { 8, 12, 1, 4, 4, 35, 74, 25, 88 };
+
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    for (int j = 0; j < nums.Length-1-i; j++)
+//    {
+
+//        if (nums[j] > nums[j + 1])
+//        {
+//            int temp = nums[j];
+//            nums[j] = nums[j + 1];
+//            nums[j + 1] = temp;          
+//        }
+//    }
+//}
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    Console.WriteLine(nums[0]);
+//}
+#endregion
